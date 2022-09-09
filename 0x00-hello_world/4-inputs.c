94e23e1bcce6d6de4133fbdd730becc455b46e7d
raselane <refiloe.raselanebothouniversity.com>
2022-09-08 19:39:05 -0700

    Adding files

---
+++ 0x00-hello_world/4-inputs.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main- Serves as the enry point for the program
+ *Return: By default returns zero if no errors otherwise returns non-zero vaalue.
+ */
+int main(void){
+  puts("\"Programming is like building a multilingual puzzle);
+return(0);
+}
